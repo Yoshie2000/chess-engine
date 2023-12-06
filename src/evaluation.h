7f4ddd3113714117a34f8db72bb7d9ce80b723e5
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-12-06 10:04:25 +0100

    Try flipping the PSQT. Bench: 4404879

--- src/evaluation.h
+++ src/evaluation.h
@@ -15,8 +15,8 @@ extern const Eval PSQ[PIECE_TYPES][64];
 
 constexpr Square psqIndex(Square square, Color side) {
     if (side == COLOR_BLACK)
-        return 63 - ((63 - square) ^ (FILE_A & RANK_8));
-    return 63 - square;
+        return 63 - square;
+    return square;
 }
 
 Eval evaluate(Board* board);
